5e9745ef22a668a8fbc34e44cbfb7e159c5f1b4c
Matthew Haas <haas@corning-cc.edu>
2026-07-08 10:45:53 -0400

    attempting to implement and test multi-line comments and pass-through comments. Not entirely working. Perhaps not working at all

---
+++ testing/comments.lua
@@ -0,0 +1,24 @@
+-- single line comment: 7777 will not be seen in the resulting assembly
+--[[ multi-line comment: 8888 will not be seen in the resulting assembly
+--   blah
+--   blah
+--   1
+--   2
+--   3
+--   and now wrapping it up --]]
+
+function main()
+
+	-- indented comment: 9999 will not be seen in the resulting assembly
+	a=5
+	b=a+4
+
+	--@ 4444 should be seen
+	--
+	--
+	c=7
+	--@[[ another multi-line comment, which should be seen
+	-- let us see
+	-- 2222 should be seen
+	-- --]]
+end
