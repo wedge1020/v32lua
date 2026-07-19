0b0c9291ae72b58f0072bf6b73a4085aad76ccd4
Matthew Haas <haas@corning-cc.edu>
2026-07-19 15:01:12 -0400

    adding working blang demo back

---
+++ demos/blang/blang-works.lua
@@ -0,0 +1,61 @@
+--#title "BLANG!"
+--#version "0.4"
+
+function init()
+    ioports.gpu.texture  = -1  -- set BIOS texture
+    player               = { }
+    player.r             = 94  -- region
+    player.x             = 315 -- x position
+    player.y             = 170 -- y position
+    player.v             = 0   -- velocity
+
+    -- enemy                = { }
+
+    ioports.gpu.clear("black") -- initial clear screen
+
+    -- draw player at initial position
+    draw(player)
+end
+
+function draw(object)
+    ioports.gpu.region   = object.r   -- set region
+    ioports.gpu.x        = object.x   -- set drawing point X
+    ioports.gpu.y        = object.y   -- set drawing point Y
+    ioports.gpu.draw()                -- draw region to screen
+end
+
+function game_loop()
+    buttonA = ioports.inp.A
+    if (buttonA >  0) then
+        player.v = 5
+    else
+        player.v = 1
+        ioports.gpu.clear("black")
+    end
+
+    left = ioports.inp.left
+    if (left >  0) then
+        player.r = 60
+        player.x = player.x - player.v
+    end
+
+    right = ioports.inp.right
+    if (right >  0) then
+        player.r = 62
+        player.x = player.x + player.v
+    end
+
+    up = ioports.inp.up
+    if (up >  0) then
+        player.r = 94
+        player.y = player.y - player.v
+    end
+
+    down = ioports.inp.down
+    if (down >  0) then
+        player.r = 86
+        player.y = player.y + player.v
+    end
+
+    draw(player)
+end
