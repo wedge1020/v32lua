7fa390d127d5e0ce123e4fd658113c6327577766
Matthew Haas <wedge@lab46.g7n.org>
2026-07-22 10:27:32 -0400

    testing pico-8 api: code compiles, assembly assembles, CART runs, but nothing appears, nor do any gamepad inputs work. Some debugging needed

--- testing/pico8api00.lua
+++ testing/pico8api00.lua
@@ -40,9 +40,13 @@ function init()
 			ioports.gpu.hotX    = x
 			ioports.gpu.minY    = y
 			ioports.gpu.hotY    = y
-			ioports.gpu.maxX    = x + 7
-			ioports.gpu.maxY    = y + 7
+			ioports.gpu.maxX    = x + 8
+			ioports.gpu.maxY    = y + 8
+
+			col                 = col + 1
+			region_id           = region_id + 1
 		end
+		row                     = row + 1
 	end
 
 	player.tile                 = 80
@@ -71,6 +75,7 @@ function game_loop()
 	--
 	-- display our player (using spr()!)
 	--
+	ioports.gpu.region          = player.tile
 	spr (player.tile + adj, player.x, player.y, 1.0, 1.0, player.xflip, player.yflip)
 
 	--
@@ -97,7 +102,7 @@ function game_loop()
 		player.yflip            = false
 	end
 
-	if (btn (right)) then
+	if (btn (right)            == true) then
 		player.tile             = 84
 		player.x                = player.x + 1
 		player.xflip            = true
@@ -108,4 +113,28 @@ function game_loop()
 	-- 60 frames a second
 	--
 	frames                      = (frames + 1) % 60
+
+	print (0,     0,   "player.x: ")
+	print (100,   0,   player.x)
+	print (0,    20,   "player.y: ")
+	print (100,  20,   player.y)
+	print (0,    40,   "texture:  ")
+	print (100,  40,   ioports.gpu.texture)
+	print (0,    60,   "region:   ")
+	print (100,  60,   ioports.gpu.region)
+	print (0,    80,   "minX:     ")
+	print (100,  80,   ioports.gpu.minX)
+	print (0,   100,   "minY:     ")
+	print (100, 100,   ioports.gpu.minY)
+	print (0,   120,   "maxX:     ")
+	print (100, 120,   ioports.gpu.maxX)
+	print (0,   140,   "maxY:     ")
+	print (100, 140,   ioports.gpu.maxY)
+	print (0,   160,   "tile:     ")
+	print (100, 160,   player.tile)
+	print (0,   180,   "btn(up):  ")
+	state = btn(up)
+	print (100, 180,   state)
+	print (0,   200,   "frames:   ")
+	print (100, 200,   frames)
 end
